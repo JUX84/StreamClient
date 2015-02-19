99b22df37abd808be2b9ed8903ac13aa80c8613e
JUX84 <mehdi.mhiri@gmail.com>
2015-02-19 11:30:52 +0100

    Update interface and stuff

--- app/src/main/java/zouxe/streamclient/MainActivity.java
+++ app/src/main/java/zouxe/streamclient/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -15,8 +16,10 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if(sp == null)
+        if(sp == null) {
             sp = new StreamPlayer(this);
+            sp.Search("", "", this);
+        }
     }
 
 
@@ -43,17 +46,48 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void search(View searchView) {
-        sp.Search("test", "test");
+
+        EditText artistText = (EditText)findViewById(R.id.artistSearchText);
+        EditText titleText = (EditText)findViewById(R.id.titleSearchText);
+
+        String artist = artistText.getText().toString();
+        String title = titleText.getText().toString();
+
+        sp.Search(artist, title, this);
+
+        titleText.setText("");
+        artistText.setText("");
+    }
+
+    public void add(View addView) {
+        EditText titleText = (EditText)findViewById(R.id.titleAddText);
+        EditText artistText = (EditText)findViewById(R.id.artistAddText);
+        String title = titleText.getText().toString();
+        String artist = artistText.getText().toString();
+        if(title.isEmpty() || artist.isEmpty() || title.equals("title") || artist.equals("artist"))
+            return;
+        sp.addSong(artist, title);
+        titleText.setText("");
+        artistText.setText("");
+        sp.Search("", "", this);
     }
 
     public void play(View controlView) {
         Button controlButton = (Button)findViewById(R.id.controlButton);
-        if(controlButton.getText().equals("Play")) {
-            sp.Play();
-            controlButton.setText("Stop");
-        } else {
-            sp.Stop();
+        if(controlButton.getText().equals("Start")) {
+            if (sp.Start())
+                controlButton.setText("Pause");
+        } else if(controlButton.getText().equals("Pause")){
+            sp.Pause();
             controlButton.setText("Play");
+        } else if(controlButton.getText().equals("Play")){
+            sp.Play();
+            controlButton.setText("Pause");
         }
+    }
+
+    public void remove(View removeView) {
+        sp.removeSong();
+        sp.Search("", "", this);
     }
 }
--- app/src/main/java/zouxe/streamclient/StreamPlayer.java
+++ app/src/main/java/zouxe/streamclient/StreamPlayer.java
@@ -5,9 +5,16 @@ import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import Player.*;
 
@@ -19,6 +26,8 @@ public class StreamPlayer implements MediaPlayer.OnPreparedListener {
     MediaPlayer mp = null;
     Player.ServerPrx server = null;
     String token = null;
+    Song[] songs = null;
+    Song selectedSong = null;
 
     public StreamPlayer(Activity activity) {
         try {
@@ -35,18 +44,33 @@ public class StreamPlayer implements MediaPlayer.OnPreparedListener {
             }
             status.setText("Connected");
             status.setTextColor(Color.GREEN);
-            server.addSong(new Song("test", "test", "test.mp3"));
             mp = new MediaPlayer();
         } catch (Ice.LocalException e) {
         }
     }
 
-    public void Play() {
+    private void selectSong(Song s) {
+        selectedSong = s;
         if(server == null)
             return;
-        token = server.selectSong(new Song("test","test","test.mp3"));
+    }
+
+    public void removeSong() {
+        if(selectedSong == null || server == null)
+            return;
+        server.removeSong(selectedSong);
+        selectedSong = null;
+    }
+
+    public Boolean Start() {
+        if(selectedSong == null || server == null)
+            return false;
+        if(token!=null)
+            server.stopSong(token);
+        token = server.selectSong(selectedSong);
         server.playSong(token);
         String mp3 = "http://zouxe.ovh:8090/"+token+".mp3";
+        mp.reset();
         mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
         mp.setOnPreparedListener(this);
         try {
@@ -55,17 +79,63 @@ public class StreamPlayer implements MediaPlayer.OnPreparedListener {
             System.out.println(e);
         }
         mp.prepareAsync();
+        return true;
     }
 
-    public void Stop() {
+    public void Pause() {
         if(server == null || token == null)
             return;
-        mp.reset();
-        server.stopSong(token);
+        mp.pause();
     }
 
-    public void Search(String artist, String title) {
-        Song[] songs = server.searchSong(artist, title);
+    public void Play() {
+        if(server == null || token == null)
+            return;
+        mp.start();
+    }
+
+    public void Search(String artist, String title, Activity activity) {
+        if(server == null)
+            return;
+
+        songs = server.searchSong(artist, title);
+
+        final String c1 = "title";
+        final String c2 = "artist";
+
+        List data = new ArrayList<HashMap<String, String>>();
+
+        for (Song s : songs) {
+            HashMap<String, String> e = new HashMap<String, String>();
+
+            e.put(c1, s.title);
+            e.put(c2, s.artist);
+            data.add(e);
+        }
+
+        SimpleAdapter adapter = new SimpleAdapter(activity,
+                data,
+                android.R.layout.simple_list_item_2,
+                new String[]{c1, c2},
+                new int[]{android.R.id.text1,
+                        android.R.id.text2});
+        ListView lv = (ListView)activity.findViewById(R.id.listView);
+        lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                selectSong(songs[position]);
+            }
+        });
+    }
+
+    public void addSong(String artist, String title) {
+        if(server == null)
+            return;
+        server.addSong(new Song(artist, title, artist+"."+title+".mp3"));
     }
 
     public void onPrepared(MediaPlayer mp) {
