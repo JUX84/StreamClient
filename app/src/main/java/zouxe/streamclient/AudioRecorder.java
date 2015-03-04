3a3abd966dfcbb04ca83d091af4fdbfdb5ae97f3
JUX84 <mehdi.mhiri@gmail.com>
2015-03-04 07:45:25 +0100

    save audio data in arraylist instead of simple array

--- app/src/main/java/zouxe/streamclient/AudioRecorder.java
+++ app/src/main/java/zouxe/streamclient/AudioRecorder.java
@@ -5,13 +5,14 @@ import android.media.AudioRecord;
 import android.media.MediaRecorder;
 
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 class AudioRecorder {
     private AudioRecord recorder = null;
     private Boolean isRecording = false;
     private Thread recordingThread = null;
-    private float[] audioData = null;
-    private int index = 0;
+    private List<Float> audioData = null;
     private static final int RECORDER_SAMPLERATE = 8000;
     private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
     private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
@@ -31,6 +32,10 @@ class AudioRecorder {
             }
         });
         recordingThread.start();
+        if(audioData == null)
+            audioData = new ArrayList<>();
+        else
+            audioData.clear();
     }
 
     public void stopRecord() {
@@ -39,19 +44,14 @@ class AudioRecorder {
         recorder.release();
         recorder = null;
         recordingThread = null;
-        index = 0;
     }
 
     private void saveAudioData() {
         while (isRecording) {
-            if(audioData == null)
-                audioData = new float[2048];
-            if(index<1024) {
-                byte[] data = new byte[BufferElements2Rec];
-                recorder.read(data, 0, BufferElements2Rec);
-                for(int i = 0; i < data.length; i+=4)
-                    audioData[index++] = ByteBuffer.wrap(data,i,4).getFloat();
-            }
+            byte[] data = new byte[BufferElements2Rec];
+            recorder.read(data, 0, BufferElements2Rec);
+            for(int i = 0; i < data.length; i+=4)
+                audioData.add(ByteBuffer.wrap(data,i,4).getFloat());
         }
     }
 }
